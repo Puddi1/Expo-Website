b4bd37c4f88ebfaf726bffc4370aec476f3d033d
Puddi1 <101431155+Puddi1@users.noreply.github.com>
2023-08-15 15:33:33 +0200

    Build skeleton

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    import "../app.postcss";
+</script>
+
+<header class="text-white">Header</header>
+<slot />
+<section class="text-white">Contact Section</section>
+<footer class="text-white">Footer</footer>
---
+++ src/routes/ghiaccio/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<main
+    class="bg-white w-screen h-screen flex items-center justify-center flex-col"
+>
+    <h1>Welcome to Expo Arredo</h1>
+    <p>
+        Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+    </p>
+    <h1 class="text-orange-800">Hello World!</h1>
+</main>
